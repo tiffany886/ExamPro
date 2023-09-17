3c74a833c4d7721937dccc3dff6414756780700f
pluto <2064623160@qq.com>
2023-09-17 11:10:55 +0800

    Initial Commit

---
+++ src/main/java/com/exampro/ExamproApplication.java
@@ -0,0 +1,13 @@
+package com.exampro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamproApplication.class, args);
+    }
+
+}
