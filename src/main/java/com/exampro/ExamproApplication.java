a72bbb642793d828dc29c025e80c2e755c3bd49e
pluto <2064623160@qq.com>
2023-09-19 01:45:43 +0800

    feat:创建数据库+记录运行问题

--- src/main/java/com/exampro/ExamproApplication.java
+++ src/main/java/com/exampro/ExamproApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExamproApplication {
 
     public static void main(String[] args) {
+        System.out.println("1123");
         SpringApplication.run(ExamproApplication.class, args);
     }
 
